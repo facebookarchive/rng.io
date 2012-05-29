f758aeb81f9265923b99509ddac56f1005cafa21
Rick Waldron <waldron.rick@gmail.com>
2012-05-29 16:46:05 -0400

    Update built files
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- dist/application.js
+++ dist/application.js
@@ -11476,15 +11476,15 @@ App.register( "fixtures", [
   },
   {
     "name": "ring-0-performance",
-    "source": "<iframe id=\"ring-0-performance\" src=\"/tests/ring-0-performance/iframe.html?10\"></iframe>\n"
+    "source": "<iframe id=\"ring-0-performance\" src=\"/tests/_resources/framerate-sprite.html?10\"></iframe>\n"
   },
   {
     "name": "ring-1-performance",
-    "source": "<iframe id=\"ring-1-performance\" src=\"/tests/ring-1-performance/iframe.html?50\"></iframe>\n"
+    "source": "<iframe id=\"ring-1-performance\" src=\"/tests/_resources/framerate-sprite.html?50\"></iframe>\n"
   },
   {
     "name": "ring-2-performance",
-    "source": "<iframe id=\"ring-2-performance\" src=\"/tests/ring-2-performance/iframe.html?100\"></iframe>\n"
+    "source": "<iframe id=\"ring-2-performance\" src=\"/tests/_resources/framerate-sprite.html?100\"></iframe>\n"
   },
   {
     "name": "selector",

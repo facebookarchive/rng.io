d0a01e67b5ac9d827ee4f09e016823406fe7e47f
Rick Waldron <waldron.rick@gmail.com>
2012-08-08 16:52:23 -0400

    Update WebRTC gUM test to allow prefixed API
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- tests/webrtc/test.js
+++ tests/webrtc/test.js
@@ -1,5 +1,12 @@
 test("WebRTC getUserMedia", function() {
-  assert( navigator.getUserMedia, "navigator.getUserMedia supported" );
+  var gUM = H.API( navigator, "getUserMedia", true );
+  assert( gUM, "navigator.getUserMedia supported" );
+});
+
+test("WebRTC getUserMedia & createObjectURL", function() {
+  var URL = H.API( window, URL, true );
+  assert( URL, "URL.create supported" );
+  assert( URL.createObjectURL, "" );
 });
 
 // TODO: translate platoon and dmv use cases into real functionality tests

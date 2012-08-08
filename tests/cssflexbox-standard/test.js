679c90793a6d7288476dd16ff5213dae4af10f58
Rick Waldron <waldron.rick@gmail.com>
2012-08-08 16:49:38 -0400

    Update CSS3 Flexbox tests to latest spec
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- tests/cssflexbox-standard/test.js
+++ tests/cssflexbox-standard/test.js
@@ -1,7 +1,37 @@
-test("CSS Flexbox, flex-align", function() {
+test("CSS Flexbox, align-content", function() {
   var div = document.createElement("div");
 
-  assert( H.test.cssProp( div, "flexAlign" ), "flexAlign supported" );
+  assert( H.test.cssProp( div, "alignContent" ), "alignContent supported" );
+});
+
+test("CSS Flexbox, align-items", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "alignItems" ), "alignItems supported" );
+});
+
+test("CSS Flexbox, align-self", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "alignSelf" ), "alignSelf supported" );
+});
+
+test("CSS Flexbox, flex", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "flex" ), "flex supported" );
+});
+
+test("CSS Flexbox, flex-basis", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "flexBasis" ), "flexBasis supported" );
+});
+
+test("CSS Flexbox, flex-direction", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "flexDirection" ), "flexDirection supported" );
 });
 
 test("CSS Flexbox, flex-flow", function() {
@@ -10,14 +40,32 @@ test("CSS Flexbox, flex-flow", function() {
   assert( H.test.cssProp( div, "flexFlow" ), "flexFlow supported" );
 });
 
-test("CSS Flexbox, flex-pack", function() {
+test("CSS Flexbox, flex-grow", function() {
   var div = document.createElement("div");
 
-  assert( H.test.cssProp( div, "flexPack" ), "flexPack supported" );
+  assert( H.test.cssProp( div, "flexGrow" ), "flexGrow supported" );
 });
 
-test("CSS Flexbox, flex-order", function() {
+test("CSS Flexbox, flex-shrink", function() {
   var div = document.createElement("div");
 
-  assert( H.test.cssProp( div, "flexOrder" ), "flexOrder supported" );
+  assert( H.test.cssProp( div, "flexShrink" ), "flexShrink supported" );
+});
+
+test("CSS Flexbox, flex-wrap", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "flexWrap" ), "flexWrap supported" );
+});
+
+test("CSS Flexbox, justify-content", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "justifyContent" ), "justifyContent supported" );
+});
+
+test("CSS Flexbox, order", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "order" ), "order supported" );
 });
--- tests/cssflexbox/test.js
+++ tests/cssflexbox/test.js
@@ -1,11 +1,37 @@
-// test("CSS Flexbox", function() {
-//   // Need baseline test
-// });
-
-test("CSS Flexbox, flex-align", function() {
+test("CSS Flexbox, align-content", function() {
   var div = document.createElement("div");
 
-  assert( H.test.cssProp( div, "flexAlign", true ), "flexAlign supported" );
+  assert( H.test.cssProp( div, "alignContent", true ), "alignContent supported" );
+});
+
+test("CSS Flexbox, align-items", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "alignItems", true ), "alignItems supported" );
+});
+
+test("CSS Flexbox, align-self", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "alignSelf", true ), "alignSelf supported" );
+});
+
+test("CSS Flexbox, flex", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "flex", true ), "flex supported" );
+});
+
+test("CSS Flexbox, flex-basis", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "flexBasis", true ), "flexBasis supported" );
+});
+
+test("CSS Flexbox, flex-direction", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "flexDirection", true ), "flexDirection supported" );
 });
 
 test("CSS Flexbox, flex-flow", function() {
@@ -14,14 +40,57 @@ test("CSS Flexbox, flex-flow", function() {
   assert( H.test.cssProp( div, "flexFlow", true ), "flexFlow supported" );
 });
 
-test("CSS Flexbox, flex-pack", function() {
+test("CSS Flexbox, flex-grow", function() {
   var div = document.createElement("div");
 
-  assert( H.test.cssProp( div, "flexPack", true ), "flexPack supported" );
+  assert( H.test.cssProp( div, "flexGrow", true ), "flexGrow supported" );
 });
 
-test("CSS Flexbox, flex-order", function() {
+test("CSS Flexbox, flex-shrink", function() {
   var div = document.createElement("div");
 
-  assert( H.test.cssProp( div, "flexOrder", true ), "flexOrder supported" );
+  assert( H.test.cssProp( div, "flexShrink", true ), "flexShrink supported" );
+});
+
+test("CSS Flexbox, flex-wrap", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "flexWrap", true ), "flexWrap supported" );
+});
+
+test("CSS Flexbox, justify-content", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "justifyContent", true ), "justifyContent supported" );
+});
+
+test("CSS Flexbox, order", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "order", true ), "order supported" );
+});
+
+
+
+
+
+
+
+
+test("CSS Flexbox, align-flow", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "alignFlow", true ), "alignFlow supported" );
+});
+
+test("CSS Flexbox, align-pack", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "alignPack", true ), "alignPack supported" );
+});
+
+test("CSS Flexbox, align-order", function() {
+  var div = document.createElement("div");
+
+  assert( H.test.cssProp( div, "alignOrder", true ), "alignOrder supported" );
 });

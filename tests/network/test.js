df926675d813fb19933acf180c8d9c8a12a0f3b6
Rick Waldron <waldron.rick@gmail.com>
2012-08-18 16:54:28 -0400

    Update network connection api to latest editor draft spec. Closes #31
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- tests/network/test.js
+++ tests/network/test.js
@@ -1,15 +1,35 @@
-test("Network Information", function() {
+test("Network Connection", function() {
   var connection = H.API( navigator, "connection", true );
 
   assert( connection, "navigator.connection supported" );
 });
 
-test("Network Information API", function() {
+test("Network Connection bandwidth", function() {
   var connection = H.API( navigator, "connection", true );
 
   if ( !connection ) {
     assert( false, "navigator.connection not supported, skipping tests" );
   } else {
-    assert( "type" in connection, "navigator.connection.type supported" );
+    assert( "bandwidth" in connection, "navigator.connection.bandwidth supported" );
+  }
+});
+
+test("Network Connection metered", function() {
+  var connection = H.API( navigator, "connection", true );
+
+  if ( !connection ) {
+    assert( false, "navigator.connection not supported, skipping tests" );
+  } else {
+    assert( "metered" in connection, "navigator.connection.metered supported" );
+  }
+});
+
+test("Network Connection onchange", function() {
+  var connection = H.API( navigator, "connection", true );
+
+  if ( !connection ) {
+    assert( false, "navigator.connection not supported, skipping tests" );
+  } else {
+    assert( "onchange" in connection, "navigator.connection.onchange supported" );
   }
 });
